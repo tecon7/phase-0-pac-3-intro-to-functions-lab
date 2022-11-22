be398b9ce98d9725655b3e42710bb5cd94132559
Teddy Economou <teddyecon@gmail.com>
2022-11-22 17:39:16 -0500

    commit tree e9e3bd252623c053c74efd7263df7ac90a059fac

---
+++ index.js
@@ -0,0 +1,31 @@
+var uppercase, lowercase;
+
+function shout(string) {
+    uppercase = string.toUpperCase();
+    return string.toUpperCase();
+} 
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    uppercase = string.toUpperCase();
+    string = uppercase;
+    console.log(uppercase);
+}
+
+function logWhisper(string) {
+    lowercase = string.toLowerCase();
+    string = uppercase;
+    console.log(lowercase);
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase() === string) {
+        return "I can\'t hear you!";
+} if (string.toUpperCase() === string){
+    return "YES INDEED!";}
+    if (string ==="Let's have dinner together!") {
+        return "I would love to!";}
+}
